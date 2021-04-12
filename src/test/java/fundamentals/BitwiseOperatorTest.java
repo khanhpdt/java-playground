d565aa82ffec077b323a48c65538bb374902fe29
pdtk <khanhpdtvn@gmail.com>
2021-04-12 23:24:30 +0700

    Refactor

--- src/test/java/fundamentals/BitwiseOperatorTest.java
+++ src/test/java/fundamentals/BitwiseOperatorTest.java
@@ -37,20 +37,26 @@ public class BitwiseOperatorTest {
 		}
 	}
 
-	private boolean throwIllegalStateException() throws Exception {
+	private boolean throwIllegalStateException() {
 		throw new IllegalStateException();
 	}
 
-	private boolean throwIllegalArgumentException() throws Exception {
+	private boolean throwIllegalArgumentException() {
 		throw new IllegalArgumentException();
 	}
 
 	@Test
-	public void testOperators() throws Exception {
-		assertThat(0b1010111 | 0b1000010, is(0b1010111));
-		assertThat(0b1010111 & 0b1000010, is(0b1000010));
-		assertThat(0b1010111 ^ 0b1000010, is(0b0010101));
-		assertThat(~ 0b1010111, is(0b1111111111111111111111111_0101000));
+	public void testOperators() {
+		assertThat(0b1010111 |
+				   0b1000010,
+				is(0b1010111));
+		assertThat(0b1010111 &
+				   0b1000010,
+				is(0b1000010));
+		assertThat(0b1010111 ^
+				   0b1000010,
+				is(0b0010101));
+		assertThat(~ 0b1010111, is(0b11111111_11111111_11111111_10101000));
 	}
 
 	@Test
@@ -61,16 +67,25 @@ public class BitwiseOperatorTest {
 		assertThat(0b10_101 >> 3, is(0b000_10));
 
 		assertThat(0b11111111111111111111111110011011, Matchers.lessThan(0));
+
+		// ">>" uses the sign bit (left most bit) to fill the trailing positions after shift.
 		assertThat(0b11111111111111111111111110011_011 >> 3, is(0b111_11111111111111111111111110011));
+
+		// ">>>" is unsigned right shift operator. It always fills 0 irrespective of the sign of the number.
 		assertThat(0b11111111111111111111111110011_011 >>> 3, is(0b000_11111111111111111111111110011));
 	}
 
 	@Test
-	public void testIdioms() throws Exception {
-		assertThat("clear all except", 0b11_1_11 & (1 << 2), is(0b00_1_00));
-		assertThat("turn off bit", 0b11_1_11 & (~(1 << 2)), is(0b11_0_11));
-		assertThat("turn on bit", 0b11_0_11 | (1 << 2), is(0b11_1_11));
-		assertThat("set bit to 0", (0b11_1_11 & (~(1 << 2))) | (0 << 2), is(0b11_0_11));
-		assertThat("set bit to 1", (0b11_0_11 & (~ (1 << 2))) | (1 << 2), is(0b11_1_11));
+	public void testIdioms() {
+		assertThat("clear all except bit ith from right to left (e.g., 2) (counting from 0)",
+				0b11_1_11 & (1 << 2), is(0b00_1_00));
+		assertThat("turn off bit ith from right to left (e.g., 2) (counting from 0)",
+				0b11_1_11 & (~(1 << 2)), is(0b11_0_11));
+		assertThat("turn on bit ith from right to left (e.g., 2) (counting from 0)",
+				0b11_0_11 | (1 << 2), is(0b11_1_11));
+		assertThat("set bit ith from right to left (e.g., 2) (counting from 0) to 0",
+				(0b11_1_11 & (~(1 << 2))) | (0 << 2), is(0b11_0_11));
+		assertThat("set bit ith from right to left (e.g., 2) (counting from 0) to 1",
+				(0b11_0_11 & (~ (1 << 2))) | (1 << 2), is(0b11_1_11));
 	}
 }
